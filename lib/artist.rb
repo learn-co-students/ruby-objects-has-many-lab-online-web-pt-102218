a7a685ea90052af84fb00c6667bcc91661193b96
Joshua D. Forrest <joshuadforrest@gmail.com>
2018-11-16 13:20:55 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,31 @@
-class artist
+class Artist
   attr_accessor :name
-  
-  def initialize
-    @name = "josh"
-    
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_song(song)
+    song.artist = self
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def self.song_count
+    Song.all.count
+  end
 end
